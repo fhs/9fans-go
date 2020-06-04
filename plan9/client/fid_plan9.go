54f7670ec86f0e3d686b96e49a9e0bb9dc4ca5a3
Fazlul Shahriar <fshahriar@gmail.com>
2020-06-04 10:49:32 -0400

    plan9: handle nil *Fid Close on plan9

--- plan9/client/fid_plan9.go
+++ plan9/client/fid_plan9.go
@@ -5,3 +5,12 @@ import "os"
 type Fid struct {
 	*os.File
 }
+
+func (f *Fid) Close() error {
+	if f == nil {
+		// The os package returns os.ErrInvalid.
+		// Return nil for p9p compatibility.
+		return nil
+	}
+	return f.File.Close()
+}
