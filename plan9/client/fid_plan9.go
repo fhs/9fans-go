5dfd473c0aa41e0c245390fb9f53d0e398ab18c1
Fazlul Shahriar <fshahriar@gmail.com>
2020-06-06 18:03:04 -0400

    plan9/client: enable zero-length writes to Fid

--- plan9/client/fid_plan9.go
+++ plan9/client/fid_plan9.go
@@ -1,6 +1,9 @@
 package client
 
-import "os"
+import (
+	"os"
+	"syscall"
+)
 
 type Fid struct {
 	*os.File
@@ -15,4 +18,18 @@ func (f *Fid) Close() error {
 		return os.ErrInvalid
 	}
 	return f.File.Close()
+}
+
+func (f *Fid) Write(b []byte) (n int, err error) {
+	if len(b) == 0 {
+		// Zero-length writes never happen in the standard library
+		// but it's useful for writes to /mnt/acme/N/data when
+		// we want to delete the text specified in /mnt/acme/N/addr.
+		//
+		// This is the workaround suggested here:
+		// https://codereview.appspot.com/7406046#msg4
+		// (commit https://go.googlesource.com/go/+/722ee1f4797a81916b19e80df479058897c44923)
+		return syscall.Write(int(f.File.Fd()), b)
+	}
+	return f.File.Write(b)
 }
